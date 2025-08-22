c3627160554b3af38194ae3a8ad31cb01622fc01
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-08-21 23:16:56 -0500

    feat: tailwindcss config

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  singleQuote: true,
-  trailingComma: "es5",
-  printWidth: 80,
-  tabWidth: 2,
-  arrowParens: "always",
-  endOfLine: "lf",
-};
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
+<script setup>
+const api = import.meta.env.VITE_API_URL;
+</script>
+
 <template>
   <div>
-    <h1>Home View</h1>
+    <h1 class="text-primary text-2xl md:text-4xl">Home View</h1>
+    <p>{{ api }}</p>
   </div>
 </template>
